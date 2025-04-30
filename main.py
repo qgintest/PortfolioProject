377232ef3ff58043a28670b78f9cca03a40d8cbe
Quality Gatekeeper <qgintest@gmail.com>
2025-04-30 13:51:14 -0400

    day 22 added titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="centered")
 
@@ -19,3 +20,15 @@ with col2:
     st.info(contents)
 
 st.write("Please see some apps below I have developed in Python to help keep your systems and devices safe")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
