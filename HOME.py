369363df0f4ceda7668f541c8402754e302dfa6f
Quality Gatekeeper <qgintest@gmail.com>
2025-05-05 15:22:49 -0400

    day 23 added page

--- HOME.py
+++ HOME.py
@@ -21,14 +21,20 @@ with col2:
 
 st.write("Please see some apps below I have developed in Python to help keep your systems and devices safe")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Future Github Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Future Github Source Code]({row['url']})")
